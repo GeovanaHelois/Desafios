d7ffcd2b768074efb70ac86c541868d268184df1
GeovanaHelois <geovana.heloiza.cordeiro@gmail.com>
2024-05-22 12:11:07 -0300

    Add files via upload

---
+++ aula/callbacks.js
@@ -0,0 +1,15 @@
+const nomes =["sthephany","maria","andressa"]
+
+nomes.forEach(function(nome){
+    console.log(nome);
+});
+
+nomes.forEach((nomes)=> {
+    console.log(nome);
+});
+
+function imprimirNome(){
+    console.log(nome);
+};
+
+nomes.forEach(imprimirNome);
---
+++ aula/map-string.js
@@ -0,0 +1,5 @@
+const nomes =["sthephany","maria","andressa"];
+
+const nomesPadronizados = nomes.map((nome) => nome.toUpperCase ());
+
+console.log(nomesPadronizados)
