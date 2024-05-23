8aa5d56231854901119895b963edb1bc71067201
GeovanaHelois <geovana.heloiza.cordeiro@gmail.com>
2024-05-23 08:35:28 -0300

    Add files via upload

---
+++ aula02/media-geral.js
@@ -0,0 +1,15 @@
+const salaJS =[7,8,8,10,6.5,4,10,7];
+const salaJAVA = [6,5,8,9,5,6];
+const salaPYTHON = [7,3.5,8,9.5];
+
+function calculaMedia (notasDasala){
+    notasDasala.reduce((acc,nota) =>{
+        returnacumulador + nota;
+    },0);
+ const media =somaDasNotas/notasDasala.length;
+return media
+};
+
+console.log (`a media da sala de JavaScript é ${calculamedia(salaJS)}`);
+console.log (`a media da sala de JavaScript é ${calculamedia(salaJS)}`);
+console.log (`a media da sala de JavaScript é ${calculamedia(salaJS)}`);
---
+++ aula02/set.js
@@ -0,0 +1,5 @@
+const nome = ["ana", "clara", "maria", "maria", "joão","joão",];
+
+const nomesAtualizados = [... new Set (nome)]
+
+console.log(meuSet);
