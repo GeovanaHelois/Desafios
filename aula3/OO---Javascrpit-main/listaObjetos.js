de5c182972e392ddc0ba7f4250bb07edf30c0e95
GeovanaHelois <geovana.heloiza.cordeiro@gmail.com>
2024-06-20 08:38:07 -0300

    Add files via upload

---
+++ aula3/OO---Javascrpit-main/listaObjetos.js
@@ -0,0 +1,14 @@
+const cliente ={
+    nome: "joão",
+    idade: 24,
+    email:"joao@cu.com",
+    telefone:["(41)9999-0000", "(41)9988-0000"]
+};
+
+cliente. endereco = 
+    
+
+rua:"r.joseph climber",
+num:1337,
+apart: true,
+complemento: "ap 934",
