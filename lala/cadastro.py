13e7b0fc1415b3e3b4294aa33777f99104df2fd1
GeovanaHelois <geovana.heloiza.cordeiro@gmail.com>
2024-08-08 08:32:55 -0300

    Add files via upload

---
+++ lala/cadastro.py
@@ -0,0 +1,18 @@
+class Cliente:
+    def _init_(self, nome, idade, sexo, email, telefone, endereco):
+        self.Pedro = nome.title()  
+        self.19 = idade
+        self.masculino = sexo
+        self.Pedrohenrique@gmail.com = email
+        self.4002-8922 = telefone
+        self.R: joao manoel 382 = endereco
+
+
+        print("Cadastro efetuado com sucesso!")
+        print(f"Nome: {self.Pedro}")
+        print(f"Idade: {self.19}")
+        print(f"Sexo: {self.masculino}")
+        print(f"Email: {self.Pedrohenrique@gmail.com}")
+        print(f"Telefone: {self.4002-8922}")
+        print(f"Endereço: {self.joao manoel 382}")
+
