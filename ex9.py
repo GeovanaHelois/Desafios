af0536e9bd06d5111d44238c40622aad52d7b3b9
GeovanaHelois <geovana.heloiza.cordeiro@gmail.com>
2024-09-13 11:08:29 -0300

    Add files via upload

---
+++ ex9.py
@@ -0,0 +1,5 @@
+
+minutos = int(input("Digite a quantidade de minutos: "))
+horas = minutos // 60
+restante_minutos = minutos % 60
+print(f"{minutos} minutos é igual a {horas} horas e {restante_minutos} minutos.")
